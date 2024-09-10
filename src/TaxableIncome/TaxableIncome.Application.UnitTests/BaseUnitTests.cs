7cf9487fe8ba2b6b356e5a1b33a61ee41d94678e
Greg Scott <greg.scott@y7mail.com>
2024-09-10 21:51:40 +1000

    Adding test project.

---
+++ src/TaxableIncome/TaxableIncome.Application.UnitTests/BaseUnitTests.cs
@@ -0,0 +1,26 @@
+// <copyright file="BaseUnitTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TaxableIncome.Application.UnitTests;
+
+using TaxableIncome.Application.Interfaces;
+
+/// <summary>
+/// The base class for unit tests in this project.
+/// </summary>
+public abstract class BaseUnitTests
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="BaseUnitTests"/> class.
+    /// </summary>
+    protected BaseUnitTests()
+    {
+        this.InputValidation = new InputValidation();
+    }
+
+    /// <summary>
+    /// Gets the instance of the input validation class for these tests.
+    /// </summary>
+    public InputValidation InputValidation { get; }
+}
---
+++ src/TaxableIncome/TaxableIncome.Application/Constants/InputConstants.cs
@@ -0,0 +1,16 @@
+// <copyright file="InputConstants.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TaxableIncome.Application.Constants;
+
+/// <summary>
+/// Input Constants for the application.
+/// </summary>
+public static class InputConstants
+{
+    /// <summary>
+    /// This is how many decimal points we want a user to define in their input.
+    /// </summary>
+    public const int AllowedDecimalPoints = 1;
+}
