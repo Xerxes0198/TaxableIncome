906a71607b90dac95f9cc498c3934cd92c5f2550
Greg <greg.scott@y7mail.com>
2024-09-10 17:52:20 +1000

    Add solution files (#2)
    * Adding Rider idea folder to ignore file.
    
    * Adding main solution file.
    
    * Resolving StyleCop Warnings.
    
    * Adding pipeline.
    
    * Moving file.

---
+++ src/TaxableIncome/TaxableIncome.Application/Program.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TaxableIncome.Application;
+
+/// <summary>
+/// Main program.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// The Main entry point of the application.
+    /// </summary>
+    /// <param name="args">Input arguments for the console app.</param>
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
