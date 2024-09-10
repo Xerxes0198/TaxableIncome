15ac6d718cb1e232886da2eef76815bc31743bdd
Greg Scott <greg.scott@y7mail.com>
2024-09-10 16:59:54 +1000

    Adding main solution file.

---
+++ src/TaxableIncome/TaxableIncome.Application/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace TaxableIncome.Application;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
