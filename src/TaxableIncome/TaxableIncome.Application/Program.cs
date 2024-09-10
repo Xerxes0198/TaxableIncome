e504ab73cf0bcd4df2f7554d1bcbd09b339553b7
Greg Scott <greg.scott@y7mail.com>
2024-09-10 17:03:30 +1000

    Resolving StyleCop Warnings.

--- src/TaxableIncome/TaxableIncome.Application/Program.cs
+++ src/TaxableIncome/TaxableIncome.Application/Program.cs
@@ -4,8 +4,15 @@
 
 namespace TaxableIncome.Application;
 
+/// <summary>
+/// Main program.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// The Main entry point of the application.
+    /// </summary>
+    /// <param name="args">Input arguments for the console app.</param>
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
